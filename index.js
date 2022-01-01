d8475743d8a0a10613ef905466c1fa5370317f96
calebjustice <caleb@ionrev.com>
2022-01-01 14:42:41 -0700

    endpoint

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
+app.post("/api", (req, res) => {
+	res.json({message: "success from server"});
+});
+
 app.use(express.static(path.join(__dirname, "client", "dist")));
+
 app.listen(8000, () => {
 	console.log("SERVER RUNNING");
 });
